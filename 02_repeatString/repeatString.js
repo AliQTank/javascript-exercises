f5922701fbddbea264f9c607a07a3117ad076f9a
alber <aicarq@hotmail.com>
2022-10-08 11:35:35 -0500

    three different methods added

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    //FOR LOOP
+    let repeatedString = [];
+    for (let i = 0; i < num; i++){
+        repeatedString.push(string) 
+    } return repeatedString.join("");
+
+    // //REPEAT METHOD
+    // return string.repeat(num);
+
+    // //WHILE LOOP
+    // var repeatedString ="";
+    // while(num > 0) {
+    //     repeatedString += string;
+    //     num--
+    // } return repeatedString;
 };
 
 // Do not edit below this line
