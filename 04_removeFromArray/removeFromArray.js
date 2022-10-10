e4f4cfd91b142e7aff4e45ab9920e23ec53bd3b3
alber <aicarq@hotmail.com>
2022-10-10 15:40:43 -0500

    indexOf function to get position of value to remove

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, valueToRemove) {
+    let newArray = [...array];
+    let valueToremoveLength = null;
+    if (!Array.isArray(valueToRemove)){
+        for (let i = 0; i <= newArray.length; i++){
+            if (newArray[i] === valueToRemove){
+                let firstPartOfArray = newArray.slice(0, newArray.indexOf(valueToRemove));
+                let secondPartOfArray = newArray.slice(newArray.indexOf(valueToRemove) + 1);
+                let concatenatedArray = firstPartOfArray.concat(secondPartOfArray);
+                return concatenatedArray;
+            }
+        }
+
+    } else {
+        return "more than one"
+    }
+    
 
 };
 
