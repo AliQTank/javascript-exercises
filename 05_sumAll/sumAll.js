997ff56c9224e92888036dbc23f7e7d8fa2d2135
alber <aicarq@hotmail.com>
2022-10-11 12:47:32 -0500

    loop way

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(firstInt, secondInt) {
+    let acumSum = 0;
+    if (Number.isInteger(firstInt) && Number.isInteger(secondInt) && firstInt > 0 && secondInt > 0) {
+        for (let i = firstInt; i <= secondInt; i++){
+            acumSum += i;
+        }
+        return acumSum;
+    }
+    
 };
 
 // Do not edit below this line
