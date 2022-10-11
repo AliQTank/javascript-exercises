2341b364619a1103ae5c0f74d754d894112d53d0
alber <aicarq@hotmail.com>
2022-10-11 16:18:21 -0500

    very carefull with return statement in brackets

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,23 +1,41 @@
-const removeFromArray = function(array, valueToRemove) {
-    let newArray = [...array];
+const removeFromArray = function(...args) {
+    let array = args[0];    
     let argumentsLength = arguments.length;
     
     if (argumentsLength === 2){
-        for (let i = 0; i <= newArray.length; i++){
-            if (newArray[i] === valueToRemove){
-                let firstPartOfArray = newArray.slice(0, newArray.indexOf(valueToRemove));
-                let secondPartOfArray = newArray.slice(newArray.indexOf(valueToRemove) + 1);
+        let valueToRemove = args[1];
+        for (let i = 0; i <= array.length; i++){
+            if (array[i] === valueToRemove){
+                let firstPartOfArray = array.slice(0, array.indexOf(valueToRemove));
+                let secondPartOfArray = array.slice(array.indexOf(valueToRemove) + 1);
                 let concatenatedArray = firstPartOfArray.concat(secondPartOfArray);
                 return concatenatedArray;
             }
         }
+    } else if (argumentsLength > 2) {
+        let newArray = [];
+        for (let i = 0; i < array.length; i++) {
+            if (!args.includes(array[i])) {
 
-    } else {
-        return "more than one"
-    }
-    
+                newArray.push(array[i]);
+            }
+        }        
+        return newArray;
+    } else {"not enough arguments"}
+}
 
-};
+// const removeFromArray = function(...args) {
+//     const array = args[0];
+//     const newArray = [];
+//     array.forEach(element => {
+//         if(!args.includes(element)){
+//             newArray.push(element);
+//         }        
+//     });    
+//     return array;
+// }
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
