83172995e3f5b7d70125cf4f3dea4407664242e9
alber <aicarq@hotmail.com>
2022-10-11 14:02:40 -0500

    ternary option

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,12 +1,16 @@
 const leapYears = function(year) {
-    if (year % 400 === 0) {
-        return true
-    } else if (year % 4 === 0) {
-        if (year % 100 ===0) {
-            return false;
-        } return true
-    }
-    return false
+    return year % 400 === 0 ? true :
+    year % 100 === 0 ? false :
+    year % 4 === 0 ? true : false;
+    
+    // if (year % 400 === 0) {
+    //     return true
+    // } else if (year % 4 === 0) {
+    //     if (year % 100 ===0) {
+    //         return false;
+    //     } return true
+    // }
+    // return false
 };
 
 // Do not edit below this line
