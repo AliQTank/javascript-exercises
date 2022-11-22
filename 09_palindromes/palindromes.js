54243fe074e700e415bcf8c167a6be08f42e15a6
alber <aicarq@hotmail.com>
2022-11-22 14:58:56 -0600

    freecodecamp exercise needs another regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,32 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const withoutSigns = /[\w]+/; 
+    let myNewString = string;
+    let myCleanArray = [];
+
+    let myFirstArray = myNewString
+    .toLowerCase()
+    .split("");
+
+    for (const letter of myFirstArray) {
+        if (withoutSigns.test(letter)) {
+            myCleanArray.push(letter);
+        }
+    }
+    
+    let myReversedArray = myNewString
+    .toLowerCase()
+    .split("")
+    .reverse();
+
+    let regexedArray = [];
+
+    for (const x of myReversedArray) {
+        if (withoutSigns.test(x)) {
+            regexedArray.push(x);
+        }
+    }
+
+    return myCleanArray.toString() === regexedArray.toString(); 
 
 };
 
