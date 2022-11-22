90567e2ac89f0b8f05c218f5ee0e9ac2f96e63c5
alber <aicarq@hotmail.com>
2022-11-22 14:56:53 -0600

    exercise completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,49 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let arraySum = 0;
+  for (let i = 0; i < array.length; i++) {
+    arraySum += array[i];    
+  }	
+  return arraySum
 };
 
-const multiply = function() {
+const sumreduceMethod = function (array) {
+  return array.reduce((total, index) => total + index, 0);
+}
 
-};
+function multiply(array) {
+  let arrayMultiplication = 1;
+  for (let i = 0; i < array.length; i++) {
+    arrayMultiplication *= array[i];
+  }
+  return arrayMultiplication;
+}
 
-const power = function() {
-	
-};
+const multiplyReduceMethod = function(array) {
+  return array.length 
+  ? array.reduce((total, indexValue) => total * indexValue,)
+  : 0;
+}
 
-const factorial = function() {
+function power(a, b) {
+  return a ** b;
+}
+
+const factorial = function(value) {
+  if (value <= 1) {
+    return 1
+  } else {
+    return value * factorial(value-1);
+  }
 	
 };
 
