fdbdef702e060e942fa6dfed38aa4a2d5fe4cad9
alber <aicarq@hotmail.com>
2022-11-23 16:30:43 -0600

    fibonacci solved, string value pending to convert for strings arguments

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(nthNumber) {
+    let myArray = [1, 1];
+    let index = +nthNumber - 1;
 
-};
+    if (index < 0) {
+        return 'OOPS'
+    } else if (index <= 1) {
+        console.log(myArray[index]);
+    } else {
+        for(let i = 1; i < index; i++)  {
+            // mySum = myArray[length];
+            myArray.push(myArray[myArray.length-2] + myArray[myArray.length-1]);
+        }
+        return myArray[index];
+    }
+
+    };
 
 // Do not edit below this line
 module.exports = fibonacci;
