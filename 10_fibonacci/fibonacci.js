f109a87940232a72275d336525f32f63f7233271
alber <aicarq@hotmail.com>
2022-11-24 09:53:43 -0600

    added an if statement to check for string values, console log erased from function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,11 +1,29 @@
 const fibonacci = function(nthNumber) {
     let myArray = [1, 1];
-    let index = +nthNumber - 1;
+    if (typeof nthNumber === 'string') {
+        let toNUmber = Number(nthNumber);
+        let index = toNUmber -1;
+
+        if (index < 0) {
+            return 'OOPS'
+        } else if (index <= 1) {
+            return myArray[index];
+        } else {
+            for(let i = 1; i < index; i++)  {
+                // mySum = myArray[length];
+                myArray.push(myArray[myArray.length-2] + myArray[myArray.length-1]);
+            }
+            return myArray[index];
+        }  
+              
+    } else {
+
+    let index = nthNumber - 1;
 
     if (index < 0) {
         return 'OOPS'
     } else if (index <= 1) {
-        console.log(myArray[index]);
+        return myArray[index];
     } else {
         for(let i = 1; i < index; i++)  {
             // mySum = myArray[length];
@@ -13,6 +31,9 @@ const fibonacci = function(nthNumber) {
         }
         return myArray[index];
     }
+        
+    }
+    
 
     };
 
